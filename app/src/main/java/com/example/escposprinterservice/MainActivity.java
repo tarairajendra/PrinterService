24d0f5a77282c45292d8275eb568c731e8fb3981
Rajendra Tarai <rajendra@channelbridge.in>
2021-01-22 12:37:08 +0530

    add onClick listner

--- app/src/main/java/com/example/escposprinterservice/MainActivity.java
+++ app/src/main/java/com/example/escposprinterservice/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.escposprinterservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button printButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        printButton = findViewById(R.id.printButton);
+        printButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
